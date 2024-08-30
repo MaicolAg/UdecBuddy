5578767b7f341a1e69d2dfd0299b6d3a9222f644
MaicolAg <maicolaa@hotmail.com>
2024-08-30 17:56:47 -0500

    modified:   home/llm/utils.py 	modified:   archivos.txt 	modified:   home/llm/__pycache__/utils.cpython-311.pyc 	modified:   home/llm/utils.py

--- home/llm/utils.py
+++ home/llm/utils.py
@@ -59,7 +59,7 @@ def text_to_pinecone(pdf_content, pdf_name):
 def create_embeddings(file_name, text):
     print(f"Creando embeddings del archivo: {file_name}")
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=2500,  # Aumenta el tamaño del fragmento
+        chunk_size=1500,  # Aumenta el tamaño del fragmento
         chunk_overlap=300,  # Reduce la superposición
         length_function=len
     )        
